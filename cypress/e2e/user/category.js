d03f355f607113ba1f0c3bdf65c5b3088ac1b701
ethan <mugoethan@gmail.com>
2023-04-16 12:32:49 +0300

    this is the code under the users module that enables us to test the add category module with adding the token as the headers from the login function and the result is the category uuid should be added and the api-url is stored also in the .env file

---
+++ cypress/e2e/user/category.js
@@ -0,0 +1,34 @@
+
+describe('API Endpoint for creating a payment under the users module', () => {
+    it('Admin should create a payment with authentication token', () => {
+      cy.fixture('category_post').then(data => {
+        cy.request({
+          method: 'POST',
+          url: `${Cypress.env('api-url')}/api/v1/category/create`,
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Bearer ${Cypress.env('userToken')}`
+          },
+          form: true,
+          body: data
+        }).then(response => {
+          expect(response.status).to.eq(200);
+          // Do something with the response
+            // Handle the login response as needed
+            // Do something with the response
+            const uuid  = response.body.data.uuid;
+            expect(uuid).to.exist;
+            cy.log(uuid);
+               // Update the token value in cypress.env.json
+          cy.readFile('cypress/fixtures/category_uuid.json').then(envFile => {
+          envFile.uuid = uuid
+          cy.writeFile('cypress/fixtures/category_uuid.json', envFile)
+        })     
+        });
+      });
+    });
+  });
+  
+
+ 
+  
