122124150efa620983c739410d3df5d5276d24c8
ethan <mugoethan@gmail.com>
2023-04-16 12:53:21 +0300

    this is the code which tests getting one product after adding creating the product and a uuid is created which is eventually used to query this route kindly create a product in postman or swagger and test this feature this product is also used to test the add to cart functionality visually

---
+++ cypress/e2e/user/product.js
@@ -0,0 +1,31 @@
+
+describe('Data', () => {
+    it('should retrieve data using the bearer token and a unique identifier', () => {
+   // define the base URL from the Cypress environment configuration
+const baseUrl = Cypress.env('api-url')
+
+// load the JSON fixture file containing the unique identifier
+cy.fixture('product_uuid.json').then(data => {
+  const uniqueIdentifier = data.uuid
+
+  // get the bearer token from the environment configuration
+  const bearerToken = Cypress.env('userToken')
+
+  // send the GET request using the base URL, unique identifier, and bearer token
+  cy.request({
+    method: 'GET',
+    url: `${baseUrl}/api/v1/product/${uniqueIdentifier}`,
+    headers: {
+      Authorization: `Bearer ${bearerToken}`
+    }
+  }).then(response => {
+    // handle the response
+        // Handle the login response as needed
+        expect(response.status).to.equal(200);
+        cy.writeFile('cypress/fixtures/get-product.json', response.body);    
+  })
+})
+
+    });
+  });
+  
