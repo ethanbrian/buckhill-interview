cd981d59dfc142187d29b04178f87c344b8bd27f
ethan <mugoethan@gmail.com>
2023-04-16 12:37:34 +0300

    this is the code under the users module that enables us to test the add payment module with adding the token as the headers from the login function and the result is the payment uuid should be added and the api-url is stored also in the .env file

---
+++ cypress/e2e/user/payment.js
@@ -0,0 +1,35 @@
+
+describe('API Endpoint for creating a payment under the users module', () => {
+    it('Admin should create a payment with authentication token', () => {
+      cy.fixture('payment.json').then(data => {
+        cy.request({
+          method: 'POST',
+          url: `${Cypress.env('api-url')}/api/v1/payment/create`,
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Bearer ${Cypress.env('userToken')}`
+          },
+          form: true,
+          body: data
+        }).then(response => {
+          expect(response.status).to.eq(200);
+          // Do something with the response
+            // Handle the login response as needed
+            // Do something with the response
+            const uuid  = response.body.data.uuid;
+            expect(uuid).to.exist;
+            Cypress.env('brand_uuid', uuid);
+            cy.log(uuid);
+               // Update the token value in cypress.env.json
+          cy.readFile('cypress/fixtures/payment_uuid.json').then(envFile => {
+          envFile.uuid = uuid
+          cy.writeFile('cypress/fixtures/payment_uuid.json', envFile)
+        })     
+        });
+      });
+    });
+  });
+  
+
+ 
+  
